59adb790abb21388661125d6f3c0a602dca3cc97
henrySilverIX <henrysilver.gold@proton.me>
2024-06-13 11:28:24 -0300

    Criei o banco de dados em MySQL com 5 tabelas. Mudei um pouco do formulário de perguntas de todos os quizzes para simplificação no código

--- database.py
+++ database.py
@@ -24,37 +24,37 @@ cursor.execute("CREATE TABLE Perguntas_final(Numero, questao, resposta)")
 resultado = cursor.execute("SELECT name FROM sqlite_master")
 resultado.fetchone()
 
-perguntas_Cap_1 = [(1, 'Defina o Scrum:', 'Framework com o qual as pessoas resolvem problemas complexos e adaptáveis, enquanto entregam produtos de forma produtiva e criativa.'),
-                   (2, 'Os valores do Scrum são:', 'II, III e V estão corretas.'),
-                   (3, 'O Scrum se baseia em 3 pilares, sendo eles:', 'Transparência, Inspeção, Adaptação.'),]
+perguntas_Cap_1 = [(1, 'Defina o Scrum:', 'Alternativa B'),
+                   (2, 'Os valores do Scrum são:', 'Alternativa C'),
+                   (3, 'O Scrum se baseia em 3 pilares, sendo eles:', 'Alternativa A'),]
 
 
-perguntas_Cap_2 = [(1, 'Qual é a função do Product Owner em um projeto ágil?', 'O Product Owner atua como uma ponte entre o cliente e o development team, apresentando as tarefas da sprint com base nos requisitos do cliente e validando o trabalho desenvolvido após sua conclusão.'),
-                   (2, 'Qual das seguintes definições corresponde a um dos estágios de um time auto-organizado?', 'Performing: É o estágio em que o time começa a operar de forma coordenada e com um desempenho satisfatório.'),
-                   (3, 'Qual a responsabilidade do Facilitador?', 'Otimizar processos, evitar que o grupo tome decisões erradas, reduzir conflitos e motivá-los.'),]
+perguntas_Cap_2 = [(1, 'Qual é a função do Product Owner em um projeto ágil?', 'Alternativa D'),
+                   (2, 'Qual das seguintes definições corresponde a um dos estágios de um time auto-organizado?', 'Alternativa A'),
+                   (3, 'Qual a responsabilidade do Facilitador?', 'Alternativa D'),]
 
-perguntas_Cap_3 = [(1, 'Verdadeiro ou Falso. Sprint podem ser definidas como:', 'I e III estão corretas.'),
-                   (2, 'A respeito do Sprint Backlog:', 'É o quadro onde terão as tarefas e subtarefas a serem desenvolvidas pelo Scrum Team, juntamente com uma ordem de prioridade, estimativa de conclusão e descrição.'),
-                   (3, 'Qual a função do Burndown Chart?', 'Tornar visível a evolução diária do trabalho do time, mostrando a comparação entre o trabalho estimado com a quantidade de esforço.'),]
+perguntas_Cap_3 = [(1, 'Verdadeiro ou Falso. Sprint podem ser definidas como:', 'Alternativa A'),
+                   (2, 'A respeito do Sprint Backlog:', 'Alternativa D'),
+                   (3, 'Qual a função do Burndown Chart?', 'Alternativa B'),]
 
 
-perguntas_Cap_final = [(1, 'O que são as cerimônias e porque são importantes?', 'As cerimônias são eventos específicos e esperados, são importantes porque ajudam a compreender a linha do tempo geral'),
-                       (2, 'O que se pode fazer com relação às estimativas quando se usa a metodologia ágil e o método Scrum?','Estimativas são sempre importantes, mas não podem ser estáticas, ser ágil significa adaptabilidade constante e encontrar novas formas de lidar com problemas complexos'),
-                       (3, 'A respeito de uma equipe que usa a filosofia ágil e a metodologia Scrum, o que se pode afirmar verdadeiramente:','Os papéis são estáveis, mas não são fixos, e à medida que os processos se desenvolvem, pessoas podem assumir novas funções, todos devem opinar e decidir, não há hierarquias'),
-                       (4, 'Assinale a afirmação verdadeira a seguir:', 'A melhor forma de começar uma Sprint Planning Meeting é apresentando a Visão do Produto'),
-                       (5, 'A respeito da Sprint Planning Meeting, pode se afirmar o seguinte:', 'É a primeira cerimônia Scrum, onde todos os detalhes precisam ser minuciosamente pensados'),
-                       (6, 'Qual o papel do Product Owner?', 'Gerenciar o Backlog, garantir o ROI, definir a visão do produto, gerenciar a entrada de requisitos e definir prioridades'),
-                       (7, 'Qual é a definição de Scrum Master no Scrum?', 'O Scrum Master é um facilitador, que ajuda a equipe a entender e adotar o Scrum, removendo impedimentos e promovendo um ambiente de trabalho colaborativo'),
-                       (8, 'Quais são as perguntas recorrentes do Daily Scrum?', 'O que fiz desde a última Daily? O que pretendo fazer até a próxima? Existe algum impedimento?'),
-                       (9, 'Qual o formato de uma User Story?', 'Como &lt tipo de usuário &gt quero &lt funcionalidade desejada &gt para quem/o/a  &lt usuário final &gt'),
-                       (10, 'Uma Release Planning Meeting é composta por:', 'Abertura - Visão do Produto - Status do Projeto - Tema - Estimativa - Mapear Itens - Plano de Riscos - Plano de Comunicação e Logística  - Gráficos - Retrospectiva'),
-                       (11, 'Qual das seguintes alternativas descreve corretamente a essência de um processo empírico no contexto do Scrum?', 'Um processo que adapta e melhora continuamente através da prática e experiência, enfrentando imprevistos'),
-                       (12, 'Qual é a importância da transparência dentro do Scrum?', 'Assegurar que todos os membros da equipe compreendam claramente os processos e estejam alinhados nos termos utilizados'),
-                       (13, 'Como o Product Owner garante que o desenvolvimento do produto está alinhado com as necessidades do negócio?', 'Definindo, priorizando e aceitando o trabalho a ser realizado pela equipe de desenvolvimento com base nas expectativas dos stakeholders'),
-                       (14, 'Qual das seguintes opções descreve corretamente a importância dos três pilares do Scrum (Transparência, Inspeção e Adaptação)?', 'Eles proporcionam um ambiente onde o progresso pode ser monitorado continuamente e ajustes podem ser feitos com base em feedback constante'),
-                       (15, 'Qual é o principal objetivo de uma User Story no Scrum?', 'Descrever uma funcionalidade desejada de forma que tenha valor para o usuário, focando no ponto de vista do cliente'),
-                       (16, 'O Scrum master não pode comparecer a daily, e você irá assumir o papel temporariamente, para que o processo não seja quebrado! Qual é as perguntas que se deve fazer aos outros membros?', 'O que fiz desde a última Daily? O que pretendo fazer até a próxima? Existe algum impedimento?'),
-                       (17, 'Dada a seguinte situação, escolha a alternativa INCORRETA: Durante a Sprint planning, todos os integrantes da equipe Scrum devem se reunir para organizar o que entregar e como produzir essa entrega de valor, respeitando o tempo da Sprint. Abaixo, estão listadas algumas dos passos para uma boa Sprint planning, escolha a alternativa INCORRETA:', 'Analizar a Sprint anterior (caso tenha) buscando melhorar e culpar os membros que atrasaram entregas')]
+perguntas_Cap_final = [(1, 'O que são as cerimônias e porque são importantes?', 'Alternativa B'),
+                       (2, 'O que se pode fazer com relação às estimativas quando se usa a metodologia ágil e o método Scrum?','Alternativa A'),
+                       (3, 'A respeito de uma equipe que usa a filosofia ágil e a metodologia Scrum, o que se pode afirmar verdadeiramente:','Alternativa D'),
+                       (4, 'Assinale a afirmação verdadeira a seguir:', 'Alternativa C'),
+                       (5, 'A respeito da Sprint Planning Meeting, pode se afirmar o seguinte:', 'Alternativa B'),
+                       (6, 'Qual o papel do Product Owner?', 'Alternativa A'),
+                       (7, 'Qual é a definição de Scrum Master no Scrum?', 'Alternativa D'),
+                       (8, 'Quais são as perguntas recorrentes do Daily Scrum?', 'Alternativa C'),
+                       (9, 'Qual o formato de uma User Story?', 'Alternativa A'),
+                       (10, 'Uma Release Planning Meeting é composta por:', 'Alternativa B'),
+                       (11, 'Qual das seguintes alternativas descreve corretamente a essência de um processo empírico no contexto do Scrum?', 'Alternativa D'),
+                       (12, 'Qual é a importância da transparência dentro do Scrum?', 'Alternativa B'),
+                       (13, 'Como o Product Owner garante que o desenvolvimento do produto está alinhado com as necessidades do negócio?', 'Alternativa A'),
+                       (14, 'Qual das seguintes opções descreve corretamente a importância dos três pilares do Scrum (Transparência, Inspeção e Adaptação)?', 'Alternativa B'),
+                       (15, 'Qual é o principal objetivo de uma User Story no Scrum?', 'Alternativa C'),
+                       (16, 'O Scrum master não pode comparecer a daily, e você irá assumir o papel temporariamente, para que o processo não seja quebrado! Qual é as perguntas que se deve fazer aos outros membros?', 'Alternativa A'),
+                       (17, 'Dada a seguinte situação, escolha a alternativa INCORRETA: Durante a Sprint planning, todos os integrantes da equipe Scrum devem se reunir para organizar o que entregar e como produzir essa entrega de valor, respeitando o tempo da Sprint. Abaixo, estão listadas algumas dos passos para uma boa Sprint planning, escolha a alternativa INCORRETA:', 'Alternativa C')]
 
 #Inserir linhas na tabela
 cursor.executemany("INSERT INTO Perguntas_Capitulo1 (Numero, questao, resposta) VALUES(?, ?, ?)", perguntas_Cap_1)
